98478634a600d409fa203d4fbe4dbabe956378cc
Rituraj Gupta <62745286+rituraj-iter@users.noreply.github.com>
2020-06-06 07:32:44 +0530

    Removed

--- ICP Assignments/Assignment 3/A3qno1.java
+++
@@ -1,9 +0,0 @@
-import  java.util.Scanner;
-public class A3qno1{
-public static void main(String args[]){
-Scanner sc=new Scanner (System.in);
-System.out.println("Enter the heigh");
-double h=sc.nextDouble();
-if(h>=6){
-System.out.println("The person is tall");
-}}}
