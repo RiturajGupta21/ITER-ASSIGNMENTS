ec62a79e832ae60a41fe37dda1e7bbd5759abed2
Rituraj Gupta <rituraj.iter@gmail.com>
2020-09-20 13:18:49 +0530

    Update A2qno17.java

--- ICP Assignments/Assignment 2/A2qno17.java
+++ ICP Assignments/Assignment 2/A2qno17.java
@@ -3,7 +3,7 @@ public static void main(String[] args) {
 int a=Integer.parseInt(args[0]);
 int b=Integer.parseInt(args[1]);
 int c=Integer.parseInt(args[2]);
-boolean s=a>=(b+c)||b>=(a+c)||c?true:false;
+boolean s=a>=(b+c)||b>=(a+c)||c>=(a+b)?true:false;
 System.out.println(s);
 }
 }
