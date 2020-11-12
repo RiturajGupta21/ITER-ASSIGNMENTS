fb6a498d2e38b87585fd635648f9ec0041ddac7d
Rituraj Gupta <rituraj.iter@gmail.com>
2020-11-12 08:34:08 +0530

    Update A4qno7.java

--- CSW-1 Assignments/Assignment 4/A4qno7.java
+++ CSW-1 Assignments/Assignment 4/A4qno7.java
@@ -4,24 +4,24 @@ public class A4qno7 {
 		//Greedy quantifier
 //		The greedy matcher would match the longest text even if parts of 
 //		matching text also match
-        Pattern p = Pattern.compile("g+");
-        Matcher m = p.matcher("ggg");
+        Pattern p = Pattern.compile(".*foo");
+        Matcher m = p.matcher("xfooxxxxxxfoo");
        while (m.find())
-            System.out.println( m.start() +" to " +(m.end()-1));
+            System.out.println("Greedy quantifier "+ m.start() +" to " +(m.end()-1));
        //Reluctant quantifier
 //       Since the quantifier is reluctant, it matches the shortest part
 //       of test with pattern. It processes one character at a time.
-       Pattern p1 = Pattern.compile("g+?");
-       Matcher m1 = p1.matcher("ggg");
+       Pattern p1 = Pattern.compile(".*?foo");
+       Matcher m1 = p1.matcher("xfooxxxxxxfoo");
       while (m1.find())
-           System.out.println( m1.start() +" to " +(m1.end()-1));
+           System.out.println("Reluctant quantifier "+ m1.start() +" to " +(m1.end()-1));
       	//Possessive quantifier
 //      This quantifier matches as many characters as it can like 
-//      greedy quantifier. But if the entire string doesn’t match, 
-//      then it doesn’t try removing characters from end.
-      Pattern p2 = Pattern.compile("g++");
-      Matcher m2 = p2.matcher("ggg");
+//      greedy quantifier. But if the entire string doesnâ€™t match, 
+//      then it doesnâ€™t try removing characters from end.
+      Pattern p2 = Pattern.compile(".*+foo");
+      Matcher m2 = p2.matcher("xfooxxxxxxfoo");
      while (m2.find())
-          System.out.println( m2.start() +" to " +(m2.end()-1));
+          System.out.println("Possessive quantifier "+ m2.start() +" to " +(m2.end()-1));
 	}
 }
