8db85c620c23f609728d61ab86bc86fc459f9339
Rituraj Gupta <rituraj.iter@gmail.com>
2021-06-28 17:24:12 +0530

    Added COA Programs

---
+++ Sem-4/COA LAB Programs/Lab9Obj2.asm
@@ -0,0 +1,18 @@
+org 100h
+mov ax,1000h
+mov ds,ax
+mov si,1500h
+mov di,1600h
+mov cl,[si]
+inc si
+mov al,[si]
+dec cl
+l1: inc si
+    mov bl,[si]  
+    cmp al,bl
+    jnc again;for greater than
+    mov al,bl
+again: dec cl
+       jnz l1
+mov [di],al
+hlt
