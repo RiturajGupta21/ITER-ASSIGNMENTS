31b8cd1c6618b4c8dd624ce540f497d40859d738
Rituraj Gupta <rituraj.iter@gmail.com>
2021-11-16 19:58:18 +0530

    Update A3Q6.py

--- Sem-5/PIP/Minor Assignment 3/A3Q6.py
+++ Sem-5/PIP/Minor Assignment 3/A3Q6.py
@@ -1,33 +1,143 @@
-def a(n):
-    print("\na\n")
-    for i in range(1, n+1):
-        for j in range(1, i+1):
-            print(j,end="")
-        print("\r")
+def pattern1():
+    for i in range(1,6):
+        for j in range(1,i+1):
+            print(j," ",end='')
+        print()
+    print()
 
-def b(n):
-    print("\nb\n")
-    for i in range(1, n+1):
-        for j in range(1, n-i+1):
-            print(end=" ")
-        for j in range(i,0,-1):
-            print(j,end="")
-        for j in range(2,i+1):
-            print(j,end="")
-        print("\r")
+def pattern2():
+    count=1
+    result='1'
+    for i in range(1,5):
+        for j in range(1,5-i):
+            print('  ',end='')
+        print(result)
+        count=count+1
+        result=str(count)+' '+result+' '+str(count)
+    print()
 
-def c(n):
-    print("\nc\n")
-    for i in range(n, 0, -1):
-        for j in range(i, 0, -1):
-            print(j, end="")
-        print("\r")  
+def pattern3():
+    for i in range(5,0,-1):
+        for j in range (i,0,-1):
+            print(j,' ',end='')
+        print()
+    print()
 
+def pattern4():
+    for i in range(1,6):
+        for j in range(1,i+1):
+            print(i," ",end='')
+        print()
+    print()
+
+def pattern5():
+    for i in range(1,6):
+        for j in range (1,i):
+            print('   ',end='')
+        for k in range (i,6):
+            print(k,' ',end='')
+        print()
         
+
+def pattern6():
+    for i in range(1,6):
+        for j in range(1,6):
+            if(i==1 or i==5 or j==1 or j==5):
+                print('* ',end='')
+            else:
+                print('  ',end='')
+        print()
+    print()
+
+def pattern7():
+    for i in range(1,6):
+        for j in range(1,6):
+            print('* ',end='')
+        print()
+    print()
+
+def pattern8():
+    result='*'
+    for i in range(1,5):
+        for j in range(1,5-i):
+            print('  ',end='')
+        print(result)
+        result='* '+result+' *'
+    print()
+
+def pattern9():
+    for i in range(1,5):
+        for j in range(1,8):
+            if(i==1 or i==j or i+j==8):
+                print('* ',end='')
+            else:
+                print('  ',end='')
+        print()
+    print()
+
+def pattern10():
+    for i in range(1,5):
+        for j in range(1,8):
+            if(i==1 or (j>=i and j<=8-i)):
+                print('* ',end='')
+            else:
+                print('  ',end='')
+        print()
+    print()
+
+def pattern11():
+    for i in range(1,8):
+        for j in range(1,8):
+            if(i+j==5 or j-i==3 or i-j==3 or i+j==11):
+                print('* ',end='')
+            else:
+                print('  ',end='')
+        print()
+    print()
+
+def pattern12():
+    for i in range(1,8):
+        for j in range(1,8):
+            if(i+j>=5 and i-j<=3 and j-i<=3 and i+j<=11):
+                print('* ',end='')
+            else:
+                print('  ',end='')
+        print()
+    print()
+
+def pattern13():
+    for i in range(1,6):
+        for j in range (1,i):
+            print('   ',end='')
+        for k in range (i,6):
+            print('$  ',end='')
+        print()
+    print()
+
+def pattern14():
+    for i in range(1,6):
+        for j in range (1,6-i):
+            print('   ',end='')
+        for k in range (1,i+1):
+            print('#  ',end='')
+        print()
+    print()
+    
 def main():
-    n = int(input("Enter number of rows: "))
-    a(n)
-    b(n)
-    c(n)
-if __name__ =="__main__":
+    pattern1()
+    pattern2()
+    pattern3()
+    pattern4()
+    pattern5()
+    pattern6()
+    pattern7()
+    pattern8()
+    pattern9()
+    pattern10()
+    pattern11()
+    pattern12()
+    pattern13()
+    pattern14()
+    
+if __name__=='__main__':
     main()
