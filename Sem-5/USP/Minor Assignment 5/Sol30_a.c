9f39be6572ad31e577bfa5cbb3cd7539471850eb
Rituraj Gupta <rituraj.iter@gmail.com>
2022-02-07 08:35:36 +0530

    Add files via upload

---
+++ Sem-5/USP/Minor Assignment 5/Sol30_a.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include<errno.h>
+#include<sys/wait.h>
+#include <unistd.h>
+int main(){
+char* args[] = {"ls", NULL};
+char* args1[] = {"wc","-l","-c",NULL};
+int a[2];
+pipe(a);
+if(!fork()){
+close(1);
+dup(a[1]);
+close(a[0]);
+execv("ls",args);
+}
+else{
+close(0);
+dup(a[0]);
+close(a[1]);
+execv("wc",args1);
+}
+}
+
---
+++ Sem-5/USP/Minor Assignment 5/Sol30_d.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include<errno.h>
+#include<sys/wait.h>
+#include <unistd.h>
+int main(){
+int a[2];
+pipe(a);
+if(!fork()){
+close(1);
+dup(a[1]);
+close(a[0]);
+execle("/bin/ls","ls",NULL,NULL);
+}
+else{
+close(0);
+dup(a[0]);
+close(a[1]);
+execle("/bin/wc","wc","-l","-c",NULL,NULL);
+}
+}
+
