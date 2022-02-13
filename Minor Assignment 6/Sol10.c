d2beee0c4d83a88c0ca147938cf8d594e577ef1c
Rituraj Gupta <rituraj.iter@gmail.com>
2022-02-13 09:49:29 +0530

    Delete Minor Assignment 6 directory

--- Minor Assignment 6/Sol10.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void){
-fprintf(stdout,"a");
-fprintf(stderr, "a has been written\n");
-fprintf(stdout,"b");
-fprintf(stderr,"b has been written\n");
-fprintf(stdout,"\n");
-return 0;
-}
